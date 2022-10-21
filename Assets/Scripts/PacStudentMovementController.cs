48c68d16aa693f0a374744ce0ef13c790d184048
Theo <theoipicard@gmail.com>
2022-10-21 21:55:34 +1100

    player movement, no collision detection

--- Assets/Scripts/PacStudentMovementController.cs
+++ Assets/Scripts/PacStudentMovementController.cs
@@ -6,7 +6,8 @@ public class PacStudentMovementController : MonoBehaviour
 {
 
     public List<AudioClip> audioClips;
-    public Vector3 movementDirection;
+    private Vector3 lastInput;
+    private Vector3 currentInput;
 
     private Tweener tweener;
     private AudioSource audioSource;
@@ -16,64 +17,93 @@ public class PacStudentMovementController : MonoBehaviour
     IEnumerator WalkInCircle() {
         while (true) {
             for (int i = 0; i < 5; i++) {
-                movementDirection = Vector3.right;
+                currentInput = Vector3.right;
                 yield return new WaitWhile(() => tweener.TweenExists(transform));
             }
             for (int i = 0; i < 4; i++) {
-                movementDirection = Vector3.up;
+                currentInput = Vector3.up;
                 yield return new WaitWhile(() => tweener.TweenExists(transform));
             }
             for (int i = 0; i < 5; i++) {
-                movementDirection = Vector3.left;
+                currentInput = Vector3.left;
                 yield return new WaitWhile(() => tweener.TweenExists(transform));
             }
             for (int i = 0; i < 4; i++) {
-                movementDirection = Vector3.down;
+                currentInput = Vector3.down;
                 yield return new WaitWhile(() => tweener.TweenExists(transform));
             }
         }
     }
 
+
+    private void readInputs() {
+        if (Input.GetKeyDown("w")) {
+            lastInput = Vector3.up;
+        }
+        else if (Input.GetKeyDown("a")) {
+            lastInput = Vector3.left;
+        }
+        else if (Input.GetKeyDown("s")) {
+            lastInput = Vector3.down;
+        }
+        else if (Input.GetKeyDown("d")) {
+            lastInput = Vector3.right;
+        }
+    }
+
+    private void checkCollisions() {
+
+    }
+
+    private void move() {
+
+            if (currentInput == Vector3.zero) {
+                animator.speed = 0;
+            } else {
+                animator.speed = 1;
+                tweener.AddTween(transform, transform.position, transform.position + currentInput, 0.25f);
+                // 0.25s to move 1 tile to line up with animation (one tile per 'waka')
+
+                if (currentInput == Vector3.right) animator.SetInteger("Direction", 0);
+                else if (currentInput == Vector3.up) animator.SetInteger("Direction", 1);
+                else if (currentInput == Vector3.left) animator.SetInteger("Direction", 2);
+                else animator.SetInteger("Direction", 3);
+
+                // Play the appropriate sound clip
+                
+                audioSource.PlayOneShot(audioClips[1]); // placeholder from assignment 3
+
+            }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        movementDirection = Vector3.zero;
+        currentInput = Vector3.zero;
         tweener = GetComponent<Tweener>();
         audioSource = GetComponent<AudioSource>();
         animator = GetComponent<Animator>();
 
         animator.speed = 0;
 
-        StartCoroutine(WalkInCircle());
+        //StartCoroutine(WalkInCircle());
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        readInputs();
+
         if (!tweener.TweenExists(transform)) {
 
-            if (movementDirection == Vector3.zero) {
-                animator.speed = 0;
-            } else {
-                animator.speed = 1;
-                tweener.AddTween(transform, transform.position, transform.position + movementDirection, 0.25f);
-                // 0.25s to move 1 tile to line up with animation (one tile per 'waka')
+            // store the direction we're currently moving so it doesn't change during the tween
+            currentInput = new Vector3(lastInput.x, lastInput.y);
 
-                if (movementDirection == Vector3.right) animator.SetInteger("Direction", 0);
-                else if (movementDirection == Vector3.up) animator.SetInteger("Direction", 1);
-                else if (movementDirection == Vector3.left) animator.SetInteger("Direction", 2);
-                else animator.SetInteger("Direction", 3);
-
-                // Play the appropriate sound clip
-                // Normally you'd check if the tile you're moving into contains a pellet or not.
-                // In this case we just play the pellet sound effect every time
-                audioSource.PlayOneShot(audioClips[1]);
-
-                // Zero out the movement direction
-                movementDirection = Vector3.zero;
-                
-            }
-            
+            checkCollisions();
+            move();
         }
+        
     }
 }
